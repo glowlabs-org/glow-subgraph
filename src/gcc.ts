80db6d1c5b432c71b8d0d851b4f0a3389c30c2fe
simon <simonnfts@gmail.com>
2023-10-17 09:32:45 -0400

    [gcc] adding retiring entities and handler

--- src/gcc.ts
+++ src/gcc.ts
@@ -1,85 +1,28 @@
 import {
-  Approval as ApprovalEvent,
-  EIP712DomainChanged as EIP712DomainChangedEvent,
   GCCRetired as GCCRetiredEvent,
-  RetireGCCAllowance as RetireGCCAllowanceEvent,
-  Transfer as TransferEvent
 } from "../generated/GCC/GCC"
 import {
-  Approval,
-  EIP712DomainChanged,
   GCCRetired,
-  RetireGCCAllowance,
-  Transfer
 } from "../generated/schema"
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleEIP712DomainChanged(
-  event: EIP712DomainChangedEvent
-): void {
-  let entity = new EIP712DomainChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
+import { User } from "../generated/schema"
 export function handleGCCRetired(event: GCCRetiredEvent): void {
   let entity = new GCCRetired(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.account = event.params.account
-  entity.rewardAddress = event.params.rewardAddress
+  let from = User.load(event.params.account.toHexString());
+  if(!from) {
+    from = new User(event.params.account.toHexString());
+    from.save();
+  }
+  let rewardAddress = User.load(event.params.rewardAddress.toHexString());
+  if(!rewardAddress) {
+    rewardAddress = new User(event.params.rewardAddress.toHexString());
+    rewardAddress.save();
+  }
+
+  entity.account = from.id;
+  entity.rewardAddress = rewardAddress.id;
   entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRetireGCCAllowance(event: RetireGCCAllowanceEvent): void {
-  let entity = new RetireGCCAllowance(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
-
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
--- tests/gcc-utils.ts
+++ tests/gcc-utils.ts
@@ -1,42 +1,9 @@
 import { newMockEvent } from "matchstick-as"
 import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
 import {
-  Approval,
-  EIP712DomainChanged,
   GCCRetired,
-  RetireGCCAllowance,
-  Transfer
 } from "../generated/GCC/GCC"
 
-export function createApprovalEvent(
-  owner: Address,
-  spender: Address,
-  value: BigInt
-): Approval {
-  let approvalEvent = changetype<Approval>(newMockEvent())
-
-  approvalEvent.parameters = new Array()
-
-  approvalEvent.parameters.push(
-    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
-  )
-  approvalEvent.parameters.push(
-    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
-  )
-  approvalEvent.parameters.push(
-    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
-  )
-
-  return approvalEvent
-}
-
-export function createEIP712DomainChangedEvent(): EIP712DomainChanged {
-  let eip712DomainChangedEvent = changetype<EIP712DomainChanged>(newMockEvent())
-
-  eip712DomainChangedEvent.parameters = new Array()
-
-  return eip712DomainChangedEvent
-}
 
 export function createGCCRetiredEvent(
   account: Address,
@@ -63,46 +30,4 @@ export function createGCCRetiredEvent(
   return gccRetiredEvent
 }
 
-export function createRetireGCCAllowanceEvent(
-  account: Address,
-  spender: Address,
-  value: BigInt
-): RetireGCCAllowance {
-  let retireGccAllowanceEvent = changetype<RetireGCCAllowance>(newMockEvent())
 
-  retireGccAllowanceEvent.parameters = new Array()
-
-  retireGccAllowanceEvent.parameters.push(
-    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
-  )
-  retireGccAllowanceEvent.parameters.push(
-    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
-  )
-  retireGccAllowanceEvent.parameters.push(
-    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
-  )
-
-  return retireGccAllowanceEvent
-}
-
-export function createTransferEvent(
-  from: Address,
-  to: Address,
-  value: BigInt
-): Transfer {
-  let transferEvent = changetype<Transfer>(newMockEvent())
-
-  transferEvent.parameters = new Array()
-
-  transferEvent.parameters.push(
-    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
-  )
-  transferEvent.parameters.push(
-    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
-  )
-  transferEvent.parameters.push(
-    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
-  )
-
-  return transferEvent
-}
